7c4e4518a2e821af68a485ac8b760bc49b34187d
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-11-07 20:21:15 +0600

    leaderboard will be created for new user with default 0 points.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
 
   # Association
   has_one :leaderboard
+
+  # After user creation, create the leaderboard with 0 points
+  after_create :create_leaderboard_with_default_points
+
+  private
+
+  def create_leaderboard_with_default_points
+    create_leaderboard!(points: 0)
+  end
 end
