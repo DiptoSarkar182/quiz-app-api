b34cd992a7224142ad46e7f3daace3fbe96fd6cb
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-11-11 15:15:04 +0600

    removed db seed from render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,7 +9,7 @@ bundle install
 bundle exec rails db:migrate
 
 # Optionally, you can also seed the database if you have seed data
- bundle exec rails db:seed
+# bundle exec rails db:seed
 
 # If you need to run any background jobs or other tasks, add them here
 # bundle exec rails jobs:work  # Example: running background jobs, if needed
