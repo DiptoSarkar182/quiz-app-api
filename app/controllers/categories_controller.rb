cdc7a14eee97d9d8fea2ac8c20db34a70cd54c06
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-11-19 22:02:20 +0600

    added create action for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,4 +12,20 @@ class CategoriesController < ApplicationController
       render json: {message: "Categories not found"}, status: 404
     end
   end
+
+  def create
+    result = Category.create_category(category_params)
+
+    if result[:status] == :ok
+      render json: { message: result[:message], data: result[:data] }, status: :created
+    else
+      render json: { message: result[:message] }, status: result[:status]
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,14 @@ class Category < ApplicationRecord
   def self.all_categories
     Category.all
   end
+
+  def self.create_category(category_params)
+    category = Category.new(category_params)
+    if category.save
+      { status: :ok, message: "Category created successfully", data: category }
+    else
+      { status: :unprocessable_entity, message: "Failed to create category", errors: category.errors.full_messages }
+    end
+  end
+
 end
